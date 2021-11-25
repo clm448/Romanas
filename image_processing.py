3154f93c6d22a382882b0660d8f33ec2565da780
clm448 <clm448@alumnos.unican.es>
2021-11-25 13:00:50 +0100

    Expanding dataset generation functionality

--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,11 @@
+def get_tif_info(filepath):
+    import gdal
+    r = gdal.Open(filepath)
+    # band = r.GetRasterBand(1)
+    (upper_left_x, x_size, x_rotation, upper_left_y, y_rotation, y_size) = r.GetGeoTransform()
+    return upper_left_x, upper_left_y
+
+
 def tiff_to_np(filepath):
     import gdal
     import numpy as np
@@ -6,24 +14,53 @@ def tiff_to_np(filepath):
     return npArray
 
 
+def check_in_range(to_check, range_):
+    matches = []
+    for elem in to_check:
+        print(elem)
+        if range_[0] < elem < range_[1]:
+            print('yes')
+            matches.append(1)
+        else:
+            print('no')
+            matches.append(0)
+    return matches
+
+
 def sliding_window(big_np_array, width, height, stride_x, stride_y):
-    from skimage.util import view_as_window
+    from skimage.util.shape import view_as_windows
     # TIFF 14200x9960   ECW 14800x10060
     window_shape = (width, height)
     stride = (stride_x, stride_y)
 
     # Crop
-    windows = view_as_window(big_np_array, window_shape, stride)
+    windows = view_as_windows(big_np_array, window_shape, stride)
     return windows
 
 
+# noinspection SpellCheckingInspection
 def get_labels(img_coordinates, size, camp_centroids, width, height, stride_x, stride_y):
+    # ------ 1 -------
     # Check which camps can be found in that image
+    # Camps centroids must be between these coordinates
+    x_range = (img_coordinates[0], img_coordinates[0]+size[0])
+    y_range = (img_coordinates[1], img_coordinates[1]+size[1])
+    # Get camps that are in those ranges
+    x_coords = camp_centroids['X']
+    y_coords = camp_centroids['Y']
 
-    # Of those camps, see which falls in which sub division
+    matches_x = check_in_range(x_coords, x_range)
+    matches_y = check_in_range(y_coords, y_range)
+
+    # Camps in desired range
+    matches = matches_x and matches_y
+
+    # ------ 2 -------
+    # Check in wich division falls each camp
     # Import o generate divisions in image
     horizontal_div = range(0, (size[0]-width+1), stride_x)
     vertical_div = range(0, (size[1]-height+1), stride_y)
+
 
     return labels
 
@@ -34,16 +71,22 @@ def labeling(images, labels):
 
 
 def trial():
+    import pandas as pd
     w = 1000
     h = 1000
     sx = 825
     sy = 896
-    image_npy = 'PNOA_CYL_SW_2009_50cm_OF_rgb_etrs89_hu30_H10_0554_1-1.npy'
-    camps_list = ''
+    size = (14200, 9960) # Cambiarlo por el size del array, aunque todos van a ser iguales
+    img_filepath = ''
+    camps_filepath = ''
+    camps_list = pd.read_csv(camps_filepath)
+
+    img_coordinates = get_tif_info(img_filepath)
+    image_npy = tiff_to_np(img_filepath)
     # Crop
     windows = sliding_window(image_npy, w, h, sx, sy)
     # Get labels
-    labels = get_labels(camps_list)
+    labels = get_labels(img_coordinates, size, camps_list, w, h, sx, sy)
     # Match image to label
     dataset = labeling(windows, labels)
 
